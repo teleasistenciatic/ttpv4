d30a0760d52adbc1fdbecca841ecc1a21fff10eb
moruge <german.moreno@magtel.es>
2015-05-19 08:43:32 +0200

    - Añadida clase SintetizadorVoz a la changelist

--- app/src/main/java/com/local/android/teleasistenciaticplus/lib/sound/SintetizadorVoz.java
+++ app/src/main/java/com/local/android/teleasistenciaticplus/lib/sound/SintetizadorVoz.java
@@ -2,51 +2,59 @@ package com.local.android.teleasistenciaticplus.lib.sound;
 
 /**
  * Created by MORUGE on 15/05/2015.
+ *
  */
 
+import java.util.Locale;
 import android.content.Context;
 import android.content.Intent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
-import android.widget.Toast;
+import android.util.Log;
 
-import java.util.Locale;
-
-public class SintetizadorVoz {
-
-    private TextToSpeech myTTS;
-    private boolean readyToSpeak = false;
+public class SintetizadorVoz implements OnInitListener
+{
+    private TextToSpeech sintetizador;
+    private boolean listoParaHablar = false;
     private Context context;
 
-    public SintetizadorVoz(Context baseContext) {
+    public SintetizadorVoz(Context baseContext)
+    {
         this.context = baseContext;
-        initOrInstallTTS();
+        sintetizador = new TextToSpeech(context, this);
     }
 
-    public void initOrInstallTTS() {
-        myTTS = new TextToSpeech(context, new OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    myTTS.setLanguage(Locale.getDefault());
-                    readyToSpeak = true;
-                } else
-                    installTTS();
-            }
-        });
+    @Override
+    public void onInit(int status)
+    {
+        if (status == TextToSpeech.SUCCESS)
+        {
+            sintetizador.setLanguage(Locale.getDefault());
+            listoParaHablar = true;
+        }
+        else
+            installTTS();
     }
 
-    private void installTTS() {
+    private void installTTS()
+    {
         Intent installIntent = new Intent();
         installIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         installIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
         context.startActivity(installIntent);
     }
 
-    public void hablaPorEsaBoquita(String text) {
-        Toast.makeText(context, text + " : " + readyToSpeak, Toast.LENGTH_SHORT).show();
-        if (readyToSpeak)
-            myTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    public void hablaPorEsaBoquita(String text)
+    {
+        if (listoParaHablar)
+            sintetizador.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        else
+            Log.i("VOZ", "Tas quedao con las ganas de oirme hablar, pringao");
     }
 
+    public void finaliza()
+    {
+        sintetizador.stop();
+        sintetizador.shutdown();
+    }
 }
